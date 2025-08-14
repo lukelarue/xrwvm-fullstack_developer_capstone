221b8c267f56f7d0541127ff9016b613c2d038cd
Luke LaRue <larvelarue@gmail.com>
2025-08-14 13:12:52 -0500

    fmt: python

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -140,8 +140,7 @@ def add_review(request):
             # response = post_review(data)
             return JsonResponse({"status": 200})
         except Exception as e:
-            return JsonResponse({"status": 401, 
-            "message": "Error in posting review"})
+            return JsonResponse({"status": 401, "message": "Error in posting review"})
     else:
         return JsonResponse({"status": 403, "message": "Unauthorized"})
 
@@ -154,6 +153,5 @@ def get_cars(request):
     car_models = CarModel.objects.select_related("car_make")
     cars = []
     for car_model in car_models:
-        cars.append({"CarModel": car_model.name, 
-        "CarMake": car_model.car_make.name})
+        cars.append({"CarModel": car_model.name, "CarMake": car_model.car_make.name})
     return JsonResponse({"CarModels": cars})
